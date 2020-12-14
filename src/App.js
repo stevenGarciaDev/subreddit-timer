30a769c0d622db73634d2e8c97a61b3303b7cfcc
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-14 08:20:19 -0800

    Add react routing and Home and Search pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Search from './pages/search';
 import './index.css';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Switch>
+        <Route path="/search" component={Search} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
