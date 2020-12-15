f67d111d6535409bbea5ff692abb99a008dfead5
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-14 19:32:13 -0800

    Utilize theme and Global Styles in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
 import Home from './pages/home';
 import Search from './pages/search';
-import './index.css';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <Normalize />
+        <GlobalStyle />
         <Switch>
           <Route path="/search" component={Search} />
           <Route path="/" component={Home} />
+          <Route>404 - Not Found</Route>
         </Switch>
       </BrowserRouter>
-    </div>
+    </ThemeProvider>
   );
 }
 
