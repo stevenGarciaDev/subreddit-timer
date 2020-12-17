149c257d97599a735c7f02f05460e4ad5c1631fe
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-17 10:42:55 -0800

    Update integration tests for header

---
+++ src/__tests__/header.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Header', () => {
+  beforeEach(() => {
+    render(
+      <App />,
+    );
+  });
+
+  it('Navigates to the home page when logo is clicked', () => {
+    const logo = screen.getByRole('link', { name: /logo\.svg/i });
+
+    userEvent.click(logo);
+
+    expect(screen.getByRole('heading', { name: 'How it works' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'About' })).toBeInTheDocument();
+  });
+
+  test.each([['Search'], ['How it works'], ['About']])(
+    '"%s" link points to the correct page',
+    (navLinkText) => {
+      const link = screen.getByRole('link', { name: navLinkText });
+
+      userEvent.click(link);
+
+      expect(
+        screen.getByRole('heading', { name: navLinkText }),
+      ).toBeInTheDocument();
+    },
+  );
+});
