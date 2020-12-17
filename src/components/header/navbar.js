f5e7afa8ee26d5674058c697f974efdddf6d6ea6
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-17 07:30:40 -0800

    Modify navbar styles to be in separate file

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -1,36 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import { NavLink, Link } from 'react-router-dom';
-import { ReactComponent as Logo } from './logo.svg';
-
-const Nav = styled.nav`
-    align-items: center;
-    display: flex;
-    height: 100px;
-    justify-content: space-between;
-    margin: 0 0 27px;
-    padding: 0 80px 0;
-    width: 100%
-`;
-
-const AppLogo = styled(Logo)`
-    display: block;
-    height: 36px;
-    width: 150px;
-`;
-
-const NavItems = styled.div`
-    position: relative;
-    left: 13px;
-`;
-
-const NavbarLink = styled(NavLink)`
-    color: ${(props) => props.theme.colors.brownishGrey};
-    font-size: 16px;
-    height: 100%;
-    padding: 0 13px;
-    text-decoration: none;
-`;
+import { Link } from 'react-router-dom';
+import {
+  Nav, AppLogo, NavItems, NavbarLink,
+} from './navbar.styles';
 
 const Navbar = () => (
   <header>
---
+++ src/components/header/navbar.styles.js
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { ReactComponent as Logo } from './logo.svg';
+
+export const Nav = styled.nav`
+    align-items: center;
+    display: flex;
+    height: 100px;
+    justify-content: space-between;
+    margin: 0 0 27px;
+    padding: 0 80px 0;
+    width: 100%
+`;
+
+export const AppLogo = styled(Logo)`
+    display: block;
+    height: 36px;
+    width: 150px;
+`;
+
+export const NavItems = styled.div`
+    position: relative;
+    left: 13px;
+`;
+
+export const NavbarLink = styled(NavLink)`
+    color: ${(props) => props.theme.colors.brownishGrey};
+    font-size: 16px;
+    height: 100%;
+    padding: 0 13px;
+    text-decoration: none;
+`;
