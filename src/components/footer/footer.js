6fb9377865cbdaa3e0a32b65088934cf1e421275
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-21 17:08:53 -0800

    Update styles for footer to ensure responsiveness

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Container, FooterLink, AnchorLink, AppLogo,
+  Container, FooterSection, FooterLink, AppLogo,
 } from './footer.styles';
 
 const Footer = () => (
   <Container>
-    <AnchorLink target="_blank" href="https://ooloo.io/employers">ooloo.io</AnchorLink>
-    <Link to="/">
-      <AppLogo />
-    </Link>
-    <FooterLink to="/terms">Terms & Privacy</FooterLink>
+    <FooterSection>
+      <FooterLink as="a" target="_blank" href="https://ooloo.io/employers">ooloo.io</FooterLink>
+      <Link to="/">
+        <AppLogo />
+      </Link>
+      <FooterLink style={{ textAlign: 'right' }} to="/terms">Terms & Privacy</FooterLink>
+    </FooterSection>
   </Container>
 );
 
--- src/components/footer/footer.styles.js
+++ src/components/footer/footer.styles.js
@@ -1,36 +1,34 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from './sign.svg';
 
-export const Container = styled.footer`
-    align-items: center;
+export const Container = styled.div`
     bottom: 0;
-    display: flex;
-    flex-direction: row;
-    height: 100px;
-    justify-content: space-around;
-    padding: 0 67px;
     position: absolute;
     width: 100%;
 `;
 
-export const LinkStyles = css`
-    color: ${(props) => props.theme.colors.brownishGrey};
-    font-size: 14px;
-    text-decoration: none;
+export const FooterSection = styled.footer`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    height: 100px;
+    justify-content: space-around;
+    padding: 0 20px;
+    margin: 0 auto;
+    max-width: 980px;
+    
 `;
 
 export const FooterLink = styled(Link)`
-    ${LinkStyles}
-`;
-
-export const AnchorLink = styled.a`
-    ${LinkStyles}
+    color: ${(props) => props.theme.colors.brownishGrey};
+    flex: 1;
+    font-size: 14px;
+    text-decoration: none;
 `;
 
 export const AppLogo = styled(Logo)`
     height: 36px;
     position: relative;
-    left: 27px;
     width: 36px;
 `;
