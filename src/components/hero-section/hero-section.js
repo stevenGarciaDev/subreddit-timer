0b7f3b825a74369a1a5398fd1b1d2d104abd23bd
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-22 18:21:43 -0800

    Add hero section component

---
+++ src/components/hero-section/hero-section.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  HeadlineSection,
+  Headline,
+  SubHeadline,
+  CallToActionButton,
+  DefaultSubreddit,
+  HeatMapLink,
+} from './hero-section.styles';
+import heatMap from './table.png';
+
+const HeroSection = () => (
+  <>
+    <HeadlineSection>
+      <Headline>No reactions to your reddit posts?</Headline>
+      <SubHeadline>
+        Great timing, great results! Find the best time to post on your subreddit.
+      </SubHeadline>
+      <CallToActionButton as={Link} to="/search/javascript">SHOW ME THE BEST TIME</CallToActionButton>
+      <DefaultSubreddit>r/javascript</DefaultSubreddit>
+    </HeadlineSection>
+    <HeatMapLink to="/search/javascript">
+      <img src={heatMap} alt="Heat Map" />
+    </HeatMapLink>
+  </>
+);
+
+export default HeroSection;
---
+++ src/components/hero-section/hero-section.styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const HeadlineSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+`;
+
+export const Headline = styled.p`
+  color: black;
+  font-family: Bitter, sans-serif;
+  font-size: 38px;
+  position: relative;
+  top: -20px;
+`;
+
+export const SubHeadline = styled.p`
+  color: #93918f;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  position: relative;
+  top: -68px;
+`;
+
+export const CallToActionButton = styled.button`
+  align-items: center;
+  color: white;
+  background-color: #fdb755;
+  border: none;
+  border-radius: 4px;
+  display: flex;
+  font-family: 'Montserrat-Medium', sans-serif;
+  font-size: 14px;
+  height: 36px;
+  justify-content: center;
+  outline: none;
+  position: relative;
+  text-decoration: none;
+  top: -40px;
+  width: 217px;
+`;
+
+export const DefaultSubreddit = styled.p`
+  color: #93918f;
+  font-family: 'Montserrat-Medium', sans-serif;
+  font-size: 16px;
+  margin: 0;
+`;
+
+export const HeatMapLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  position: relative;
+  top: 32px;
+`;
