d2a673baf85c73a9b9aa8a43c721ff30d99f33d5
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-28 18:33:11 -0800

    Modify info section to use HTML5 semtantic, article

--- src/components/info-section/info-section.styles.js
+++ src/components/info-section/info-section.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const Container = styled.section`
+export const Container = styled.article`
     display: flex;
     flex-direction: column;
     align-items: center;
