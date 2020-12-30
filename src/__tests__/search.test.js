caee68769e993f4ad5529a7e42ba5d143a275658
Steven Garcia <stevgarcia2015@gmail.com>
2020-12-29 21:33:35 -0800

    Add integration test for Search component

---
+++ src/__tests__/search.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route } from 'react-router-dom';
+import App from '../App';
+
+const setup = (initialPath = '/') => {
+  let history;
+  render(
+    <MemoryRouter initialEntries={[initialPath]}>
+      <App />
+      <Route
+        path="*"
+        render={(props) => {
+          history = props.history;
+          return null;
+        }}
+      />
+    </MemoryRouter>,
+  );
+  return { history };
+};
+
+describe('Search page', () => {
+  it('Successfully updates URL route parameter when user clicks the search button', () => {
+    const { history } = setup('/search/javascript');
+
+    const subredditInput = screen.getByLabelText('r /');
+    fireEvent.change(subredditInput, { target: { value: 'reactjs' } });
+
+    const submitButton = screen.getByRole('button', { name: /search/i });
+    userEvent.click(submitButton);
+
+    expect(history.location.pathname).toEqual('/search/reactjs');
+  });
+});
