e3af3e2aee6ac7e8775712c22c0ade23bb3c2257
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-05 12:03:08 -0800

    Refactor subreddit service API call to use a recursive implementation

--- src/services/subredditService.js
+++ src/services/subredditService.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-console */
-export default async function getSubredditPosts(subreddit) {
+export default async function fetchPaginatedPosts(subreddit, previousPosts = [], after = '') {
   try {
-    let posts = [];
-    const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=500`);
-    const { data } = await response.json();
-    posts = data.children;
-    return posts;
+    let apiUrl = `https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100`;
+    if (after) {
+      apiUrl += `&after=${after}`;
+    }
+    const response = await fetch(apiUrl);
+    const { data: { children: posts, after: newAfter } } = await response.json();
+    const combinedPosts = [...previousPosts, ...posts];
+    const maxAmountOfPostsToCombine = 500;
+    if (posts.length < 100 || combinedPosts.length >= maxAmountOfPostsToCombine) {
+      return combinedPosts;
+    }
+    return fetchPaginatedPosts(subreddit, combinedPosts, newAfter);
   } catch (error) {
     console.log('Unable to fetch Subreddit Posts', error);
     return [];
